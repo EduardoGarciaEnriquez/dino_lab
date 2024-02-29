4b11f65ef4ea2f3db1da6bf8c26409f5989fb4f1
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-02-29 11:59:50 -0600

    ci: header style, logo and drawer.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,67 +1,82 @@
 'use client'
-import React, {useState} from "react";
-import Link from "next/link";
+import React, { useState } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
-import Button from "@mui/material/Button";
-import { Drawer } from "@mui/material";
+import Button from '@mui/material/Button'
+import { Drawer, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import { IoMdMenu, IoIosClose, IoIosBook, IoIosImages, IoMdPeople } from 'react-icons/io'
+import Logo from '../../app/favicon.ico'
 
 function Header() {
-    const [open, setOpen] = useState<boolean>(false)
+  const [open, setOpen] = useState<boolean>(false)
 
   return (
-    <header className="flex items-center justify-between px-4 sm:px-6 lg:px-8 py-4 border-b border-gray-200 dark:border-gray-800">
-      <Link
-        className="flex items-center space-x-2 text-xl font-medium"
-        href="#"
-      >
-        <SquareIcon className="w-6 h-6" />
+    <header className='flex items-center justify-between px-4 sm:px-6 lg:px-8 py-4 border-b border-gray-200 dark:border-gray-800'>
+      <Link className='flex items-center space-x-2 text-xl font-medium' href='/'>
+      <Image
+          alt='Logo'
+          src={Logo}
+          width={40}
+          height={40}
+        />
       </Link>
-      <nav className=" hidden md:flex items-center gap-4 text-sm font-medium">
-        <Link
-          className="text-gray-900 hover:underline dark:text-gray-50"
-          href="#"
-        >
+      <nav className=' hidden md:flex items-center gap-4 text-sm font-medium'>
+        <Link className='text-gray-900 hover:underline dark:text-gray-50' href='#'>
           Catalogo
         </Link>
-        <Link
-          className="text-gray-900 hover:underline dark:text-gray-50"
-          href="#"
-        >
+        <Link className='text-gray-900 hover:underline dark:text-gray-50' href='#'>
           Acerca de nosotros
         </Link>
-        <Link
-          className="text-gray-900 hover:underline dark:text-gray-50"
-          href="#"
-        >
+        <Link className='text-gray-900 hover:underline dark:text-gray-50' href='#'>
           Blog
         </Link>
       </nav>
 
-      <Button onClick={() => setOpen(true)}>menu</Button>
-      <Drawer open={open} onClose={() => setOpen(false)}>
-        <p>opc 1</p>
+      <Button className='text-black text-2xl p-0 md:hidden' onClick={() => setOpen(true)}>
+        <IoMdMenu />
+      </Button>
+
+      <Drawer anchor='right' open={open} onClose={() => setOpen(false)}>
+        <div className='flex justify-end'>
+          <Button className='text-black text-3xl' onClick={() => setOpen(false)}>
+            <IoIosClose />
+          </Button>
+        </div>
+
+        <Box sx={{ minWidth: 320, width: '100vw', maxWidth: 425, bgcolor: 'background.paper' }}>
+          <nav aria-label='main mailbox folders'>
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <IoIosImages className='text-black text-3xl' />
+                  </ListItemIcon>
+                  <ListItemText primary='Catalogo' />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <IoMdPeople className='text-black text-3xl' />
+                  </ListItemIcon>
+                  <ListItemText primary='Acerca de nosotros' />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <IoIosBook className='text-black text-3xl' />
+                  </ListItemIcon>
+                  <ListItemText primary='Blog' />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </nav>
+        </Box>
       </Drawer>
     </header>
-  );
+  )
 }
 
-function SquareIcon(props: React.SVGProps<SVGSVGElement>): JSX.Element {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <rect width="18" height="18" x="3" y="3" rx="2" />
-    </svg>
-  );
-}
-
-export default Header;
+export default Header
