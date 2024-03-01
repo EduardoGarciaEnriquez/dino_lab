95ff5f993212d58009f9f1be417584b3148b6285
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-02-29 22:10:07 -0600

    add: carousel with material ui.

--- src/components/ui/carousel/index.tsx
+++ src/components/ui/carousel/index.tsx
@@ -1,48 +1,14 @@
-import Image from "next/image";
+'use client'
+import React, { ReactNode } from 'react'
 
-import React, { useState } from 'react';
+import Carousel from 'react-material-ui-carousel'
 
-interface CarouselProps {
-  images: string[];
+function Slider({children}: {children: ReactNode}) {
+  return (
+    <Carousel animation='slide'>
+      {children}
+    </Carousel>
+  )
 }
 
-const Carousel: React.FC<CarouselProps> = ({ images }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const goToPrevious = () => {
-    setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : images.length - 1));
-  };
-
-  const goToNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex < images.length - 1 ? prevIndex + 1 : 0));
-  };
-
-  return (
-    <div className="relative">
-      <div className="flex justify-center">
-        <Image
-                alt="Image"
-                className="w-full max-w-md rounded-lgmx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full"
-                src={images[currentIndex]}
-                width={550}
-                height={310}
-                layout="responsive"
-              />      
-      </div>
-      <button
-        onClick={goToPrevious}
-        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
-      >
-        Prev
-      </button>
-      <button
-        onClick={goToNext}
-        className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
-      >
-        Next
-      </button>
-    </div>
-  );
-};
-
-export default Carousel;
+export default Slider
