0926fc2f06575f2be162f0b34629f7c87661e008
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-03-04 21:48:13 -0600

    ci: reusable main icon

---
+++ src/components/ui/dino-lab-icon/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import Logo from '../../../app/favicon.ico'
+
+function DinoLabIcon() {
+  return (
+    <Link className='flex items-center space-x-2 text-xl font-medium' href='/'>
+      <Image
+          alt='Logo'
+          src={Logo}
+          width={40}
+          height={40}
+        />
+      </Link>
+  )
+}
+
+export default DinoLabIcon
