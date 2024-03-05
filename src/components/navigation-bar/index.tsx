21e3bd3337b4276baf377d54142862d6e347b123
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-03-04 22:19:35 -0600

    ci: rename header to navigation bar due to functionality.

---
+++ src/components/navigation-bar/index.tsx
@@ -0,0 +1,13 @@
+import NavigationTop from './navigation-top'
+import NavigationBottom from './navigation-bottom'
+
+function NavigationBar() {
+  return (
+    <>
+      <NavigationTop />
+      <NavigationBottom />
+    </>
+  )
+}
+
+export default NavigationBar
