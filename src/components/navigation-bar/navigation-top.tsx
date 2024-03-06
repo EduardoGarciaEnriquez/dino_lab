9645db4c4279f418441d7298ae403add80eeee44
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-03-06 13:45:04 -0600

    feat: dark & light themes

--- src/components/navigation-bar/navigation-top.tsx
+++ src/components/navigation-bar/navigation-top.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import Link from 'next/link'
 
-import { Button } from '@mui/material'
 import { IoLogoWhatsapp } from 'react-icons/io'
 
 import DinoLabIcon from '../ui/dino-lab-icon'
+import ToggleTheme from '../ui/toggle-theme'
 
 function NavigationTop() {
-
   return (
-    <header className='hidden md:flex items-center justify-between px-4 py-4 border-b border-gray-200 fixed top-0 w-full z-10 bg-white 
-    dark:bg-gray-950 dark:text-gray-50 dark:border-gray-800'>
+    <header
+      className='hidden md:flex items-center justify-between px-4 py-4 border-b border-gray-200 fixed top-0 w-full z-10 bg-white 
+    dark:bg-gray-950 dark:text-gray-50 dark:border-gray-800'
+    >
       <DinoLabIcon />
       <nav className='hidden md:flex items-center gap-4 text-sm font-medium'>
-    
-          <Button className='text-gray-900 hover:underline dark:text-gray-50'>Blog</Button>
+        <ToggleTheme />
 
-        <Link className='text-green-600 text-3xl animate-bounce hover:animate-none' href='https://wa.me/523334946342?text=¡Hola!%20Quiero%20más%20información%20sobre%20los%20servicios%20de%20DinoLab' target='_blank'>
-          <IoLogoWhatsapp/>
+        <Link
+          className='text-green-600 text-3xl animate-bounce hover:animate-none'
+          href='https://wa.me/523334946342?text=¡Hola!%20Quiero%20más%20información%20sobre%20los%20servicios%20de%20DinoLab'
+          target='_blank'
+        >
+          <IoLogoWhatsapp />
         </Link>
       </nav>
     </header>
