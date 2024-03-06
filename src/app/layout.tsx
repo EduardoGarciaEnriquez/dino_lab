fd026184470f56cdc866b2a4d9a5e5803d47c912
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-03-06 14:10:21 -0600

    fix: suspense error on deployment due to searchParams

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Suspense } from 'react'
 
 import ClientLayout from './client-layout'
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <ClientLayout>{children}</ClientLayout>
+      <Suspense>
+        <ClientLayout>{children}</ClientLayout>
+      </Suspense>
     </html>
   )
 }
